363646496139f897dc112fcd894cef7ed5139164
Matheus <matheuscesardelimasantoss@gmail.com>
2026-03-26 22:12:29 -0300

    aula2603

---
+++ aula02/funcoes.js
@@ -0,0 +1,28 @@
+// Pascal Case é um padrão de escrita que separa as
+// palavras onde toda a setença inicia em maiusculo.
+
+function SomaNumeros () {
+let valor_i = 6
+let valor_ii = 5
+let resultado = 0
+
+return resultado = valor_i + valor_ii
+
+}
+console.log(SomaNumeros())
+
+
+
+function SomaNumerosII (valor_i, valor_ii) {
+console.log (valor_i + valor_ii)
+
+}
+SomaNumerosII(9, 15)
+SomaNumerosII(3, 4)
+SomaNumerosII(90, 50)
+
+const SOMA_NUMEROS_III = (valor_i, valor_ii) => {
+return valor_i + valor_ii
+}
+
+console.log(SOMA_NUMEROS_III(81, 98))
